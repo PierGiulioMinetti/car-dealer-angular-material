540cba4c6c069b71fe0afdc8f7c7a8bf7df08aca
Pier Giulio Minetti <piergiulio.minetti@alkemy.com>
2022-12-16 16:18:56 +0100

    crud methods created

--- src/app/core/services/car.service.ts
+++ src/app/core/services/car.service.ts
@@ -10,7 +10,26 @@ export class CarService {
 
   constructor(private http: HttpClient) { }
 
+  //GET - all the cars
   fetchCars() {
     return this.http.get<Car[]>(this.URL);
   }
+
+  //GET - single car
+
+  //POST
+  createCar(car: Car) {
+    return this.http.post<Car>(this.URL, car)
+  }
+  //PUT
+  editCar(id: number, car: Car) {
+    return this.http.patch<Car>(`${this.URL}/${id}`, car)
+
+  }
+  //DELETE
+  deleteCar(id: number) {
+    return this.http.delete<Car>(`${this.URL}/${id}`)
+  }
+
+
 }
