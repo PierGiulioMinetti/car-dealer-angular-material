9bad5d2acd9a27b4159c2314555b34b5f466e60f
Pier Giulio Minetti <piergiulio.minetti@alkemy.com>
2022-12-16 12:46:59 +0100

    style added to buttons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,19 @@
+// MODULES
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// COMPONENTS
 import { CardComponent } from './components/card/card.component';
+// MATERIAL
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 const COMPONENTS = [
   CardComponent
 ];
 
 const MODULES = [
-  MatCardModule
+  MatCardModule,
+  MatButtonModule
 ]
 
 @NgModule({
